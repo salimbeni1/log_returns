3183b09754e1b9a62fd844fb9d4cce15e9311e9b
Filippo888 <filippo.salmian@epfl.ch>
2022-04-25 16:41:31 +0200

    solve script error

--- front-end/pages/_app.js
+++ front-end/pages/_app.js
@@ -2,10 +2,11 @@
 import '../styles/globals.css'
 import styles from '../styles/Home.module.scss'
 import Link from 'next/link'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
   return <div className={styles.ctn}>
-<script src="https://www.gstatic.com/charts/loader.js"></script>
+<Script src="https://www.gstatic.com/charts/loader.js"></Script>
     <header className={styles.header}>
       <h1 style={ {color: "white"}}>Log-Returns</h1> 
       <div style={ {height: "60%", width: "3px", backgroundColor: "var(--font-color-1)" , marginRight:"20px"}}></div> 
